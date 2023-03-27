0357a51bc226bf93b1b470852fc9ee018af2ff3c
iamvijaysubramanian <vijaysubbu69@gmail.com>
2023-03-27 19:33:48 +0530

    first commit

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-light text-light p-4">
+      <h6 className="text-center">All rights reserved &copy; vijaysubramanian</h6>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Login.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { Form, Input, message, Button } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  //from submit
+  const submitHandler = async (values) => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post("http://localhost:8080/api/v1/users/login", values);
+      setLoading(false);
+      message.success("login success");
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ ...data.user, password: "" })
+      );
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      message.error("something went wrong");
+    }
+  };
+
+  //prevent for login user
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      navigate("/");
+    }
+  }, [navigate]);
+  return (
+    <>
+      <div className="resgister-page ">
+        {loading && <Spinner />}
+        <Form layout="vertical" onFinish={submitHandler}>
+          <h1>Login Form</h1>
+
+          <Form.Item label="Email" name="email">
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" />
+          </Form.Item>
+          
+          <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        <Link to="/register"> Not a user? Click here to Register now!</Link>
+      </Form.Item> 
+          
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
