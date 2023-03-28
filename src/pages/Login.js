7d943dc0663a18c4dbeb4a542abf3374fcbaebec
iamvijaysubramanian <vijaysubbu69@gmail.com>
2023-03-28 15:32:53 +0530

    commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
   const submitHandler = async (values) => {
     try {
       setLoading(true);
-      const { data } = await axios.post(`${process.env.REACT_APP_.BASE_URL}/users/login`, values);
+      const { data } = await axios.post(`${process.env.REACT_APP_BASE_URL}/users/login`, values);
       setLoading(false);
       message.success("login success");
       localStorage.setItem(
